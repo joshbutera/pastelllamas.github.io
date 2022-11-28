42760645578c96a0b8cf37c445df6329f54231eb
joshbutera <joshuafbutera@gmail.com>
2022-11-28 16:34:29 -0600

    bar chart transition

--- barchart.js
+++ barchart.js
@@ -1,9 +1,10 @@
 function drawBarChart(barVar, dataset) {
-   year = 2015
-   color_filter = barVar
-   console.log(color_filter, dataset)
-   bar_filter = "happiness_score"
+   var year = 2015
+   var color_filter = barVar
+   var bar_filter = "happiness_score"
    dataset[year].sort((a, b) => a[bar_filter] - b[bar_filter])
+   var t = d3.transition()
+      .duration(500);
    var colorScale = d3.scaleLinear().domain([d3.min(dataset[year], d => d[color_filter]), d3.max(dataset[year], d => d[color_filter])]).range([min_color, max_color])
    var yScale = d3.scaleLinear().domain([0, d3.max(dataset[year], d => d[bar_filter])] ).range([600 - margin.top - margin.bottom, 0])
    var xScale = d3.scaleBand().domain(d3.map(dataset[year], d => d["Country"]) ).range([0, getWidth()-margin.right-margin.left]).padding(0.4)
@@ -13,25 +14,41 @@ function drawBarChart(barVar, dataset) {
    var yAxis = d3.axisLeft()
    .scale(yScale);
 
+   d3.selectAll('.bar-axis').remove()
    bar_chart.append("g")
-   .attr("class", "axis")
+   .attr("class", "bar-axis")
    .attr("transform", "translate(0, " + (600-margin.top-margin.bottom) + ")")
    .call(xAxis)
 
    bar_chart.append("g")
-   .attr("class", "axis")
+   .attr("class", "bar-axis")
    .call(yAxis)
 
-   d3.selectAll(".axis > g").remove()
+   d3.selectAll(".bar-axis > g").remove()
 
    bar_chart.selectAll("rect")
       .data(dataset[year])
+      .join(
+         enter => enter.append('rect')
+            .transition()
+            .delay((d, i) => 10 * i)
+            .duration(600)
+            .attr("x", d => xScale(d.Country))
+            .attr("y", d => yScale(d[bar_filter]))
+            .attr("width", xScale.bandwidth())
+            .attr("height", d => 600-margin.top-margin.bottom - yScale(d[bar_filter]))
+            .attr("border", "1px solid black")
+            .attr("fill", d => colorScale(d[color_filter])),
+         update => update
+            .transition(t)
+            .attr("fill", function(d) { 
+               console.log('test')
+               return colorScale(d[color_filter])
+            }),
+         exit => exit
+            .remove()
+
+      )
       .enter()
          .append("rect")
-         .attr("x", d => xScale(d.Country))
-         .attr("y", d => yScale(d[bar_filter]))
-         .attr("width", xScale.bandwidth())
-         .attr("height", d => 600-margin.top-margin.bottom - yScale(d[bar_filter]))
-         .attr("border", "1px solid black")
-         .attr("fill", d => colorScale(d[color_filter]))
 }
